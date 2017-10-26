3ec6f587c5801b68262834a2e4244320e941a83c
Surfboy678 <Surfboy678@users.noreply.github.com>
2017-10-26 13:39:20 +0200

    Commit

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -4,9 +4,7 @@ import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,27 +16,32 @@ public class TaskController {
     private DbService service;
     @Autowired
     private TaskMapper taskMapper;
-    @RequestMapping(method = RequestMethod.GET, value = "getTask")
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto>getTasks(){
         return taskMapper.mapToTaskDtoList(service.getAllTask());
     }
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
-    public List<TaskDto> getTask(){
-        return new ArrayList<>();
-    }
+
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto getTask(String taskId){
+    public List<TaskDto> getTask(){
+       return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTask_duplicated")
+    public TaskDto getTask(@RequestParam Long taskId){
         return new TaskDto((long)1,"test title","test_content");
     }
+
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
-    public void deleteTask(String taskId){
+    public void deleteTask(@RequestParam Long taskId){
 
     }
     @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
-    public TaskDto updateTask(TaskDto taskDto){
+    public TaskDto updateTask(@RequestBody TaskDto taskDto){
         return new TaskDto((long)1,"Edited task title", "Test content");
 
     }
+
     @RequestMapping(method = RequestMethod.POST, value = "createTask")
     public  void createTask(TaskDto taskDto){
 
--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity(name="task")
+@Entity(name="tasks")
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
