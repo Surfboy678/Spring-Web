b725eb146da1fa34b4f29ee878ead2cd7ac0c5f1
Surfboy678 <Surfboy678@users.noreply.github.com>
2017-10-30 22:09:20 +0100

    Commit

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -26,7 +26,7 @@ public class TaskController {
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public List<TaskDto> getTask(){
-       return new ArrayList<>();
+        return new ArrayList<>();
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask_duplicated")
@@ -46,7 +46,7 @@ public class TaskController {
 
     @RequestMapping(method = RequestMethod.POST, value = "createTask", consumes = APPLICATION_JSON_VALUE)
     public void createTask(@RequestBody TaskDto taskDto) {
-       service.saveTask(taskMapper.mapToTask(taskDto));
+        service.saveTask(taskMapper.mapToTask(taskDto));
     }
 
 }
