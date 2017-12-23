82a62c345a70102643a5d892cd9c94192dab1e52
Surfboy678 <Surfboy678@users.noreply.github.com>
2017-12-24 00:15:51 +0100

    Commit

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import org.slf4j.Logger;
@@ -19,16 +21,29 @@ public class SimpleEmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
-    public void send(final Mail mail){
+    @Autowired
+    private MailCreatorService mailCreatorService;
+
+    public void send(final Mail mail) {
         LOGGER.info("Starting email preparation...");
         try {
             javaMailSender.send(createMaiMessage(mail));
             LOGGER.info("Email has been set.");
-        }catch (MailException e){
-            LOGGER.error("Failed to process email sending",e.getMessage(), e);
+        } catch (MailException e) {
+            LOGGER.error("Failed to process email sending", e.getMessage(), e);
 
         }
     }
+
+        public MimeMessagePreparator createMimeMessageNumberOfTasks(final Mail mail){
+            return mimeMessage -> {
+                MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+                messageHelper.setTo(mail.getMailTo());
+                messageHelper.setSubject(mail.getSubject());
+                messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()),true);
+            };
+        }
+
 
     private SimpleMailMessage createMaiMessage(final Mail mail){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
